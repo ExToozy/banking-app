11780831ea0ef08be37ec626ed2c5eb0eb206761
ExToozy <ilya.makarov.04@mail.ru>
2024-12-01 17:14:46 +0700

    add object to json converter

---
+++ src/main/java/ru/extoozy/bankingapp/util/ObjectConverter.java
@@ -0,0 +1,28 @@
+package ru.extoozy.bankingapp.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+@Component
+@Converter
+@RequiredArgsConstructor
+public class ObjectConverter implements AttributeConverter<Object, String> {
+
+    public final ObjectMapper mapper;
+
+    @SneakyThrows
+    @Override
+    public String convertToDatabaseColumn(Object attribute) {
+        return mapper.writeValueAsString(attribute);
+    }
+
+    @SneakyThrows
+    @Override
+    public Object convertToEntityAttribute(String dbData) {
+        return mapper.convertValue(dbData, Object.class);
+    }
+}
