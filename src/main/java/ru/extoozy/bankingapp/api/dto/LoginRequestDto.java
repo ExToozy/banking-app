d594e94eda85c7449d8585a9801049310e8e318c
ExToozy <ilya.makarov.04@mail.ru>
2024-12-02 20:15:44 +0700

    fix dto

--- src/main/java/ru/extoozy/bankingapp/api/dto/LoginRequestDto.java
+++ src/main/java/ru/extoozy/bankingapp/api/dto/LoginRequestDto.java
@@ -1,7 +1,13 @@
 package ru.extoozy.bankingapp.api.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     @NotNull(message = "Username must not be null")
